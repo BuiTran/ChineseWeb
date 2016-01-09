1821d04d444fda72f383d6acd510648aadeee69c
Renata1995 <zchai13@austincollege.edu>
2016-01-09 06:54:13 -0600

    Add lessons to bootstrap

--- grails-app/domain/chineseweb/Lesson.groovy
+++ grails-app/domain/chineseweb/Lesson.groovy
@@ -3,12 +3,12 @@ package chineseweb
 class Lesson {
 	String lessonNo
 	String lessonTitle
+	String courseCode
 	
 	static belongsTo = Course
 	
     static constraints = {
 		lessonNo nullable: false
 		lessonTitle nullable: false
-		courseCode nullable: false
     }
 }
