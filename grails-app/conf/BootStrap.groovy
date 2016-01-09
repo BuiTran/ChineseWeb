35b6b775245887b45b0ffc78ee77641df2260cf1
Tran Bui <tran.n.bui@gmail.com>
2016-01-08 21:12:22 -0800

    trying to manually merge with renata branch

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,5 +1,6 @@
-import chineseweb.Flashcard
-import chineseweb.Lesson
+import chineseweb.*
+import chineseweb.security.*
+
 class BootStrap {
 
     def init = { servletContext ->
@@ -7,7 +8,41 @@ class BootStrap {
 		def f1 = new Flashcard(symbol: "ma", pronunciation: "ma", definition: "idk")
 		f1.save(flush: true)
 		
+		
+		User ablock=new User(username:"ablock",password:"ablock").save(flush:true)
+		User mhiggs=new User(username:"mhiggs",password:"mhiggs").save(flush:true)
+		Role admin=new Role(authority:"ROLE_ADMIN",enabled:true).save(flush:true)
+		UserRole.create(ablock,admin,true)
+		UserRole.create(mhiggs,admin,true)
+
+		/*Create courses for each user*/
+		def list=[110,120,210,310,410]
+		
+		list.each{level->
+			Course c=randomCourse(level)
+			c.courseCode=level+"A"
+			ablock.addToCourses(c).save(flush:true,failOnError:true)
+			c.save(flush:true,failOnError:true)
+		}
+		
+		list.each{level->
+			Course c=randomCourse(level)
+			c.courseCode=level+"B"
+			mhiggs.addToCourses(c).save(flush:true,failOnError:true)
+			c.save(flush:true,failOnError:true)
+		}
     }
+	
+	/*
+	 * Generate a random course
+	 */
+	def randomCourse(int num){
+		Course c=new Course()
+		c.courseTitle=nameList.getAt(rand.nextInt(nameList.size()))+num.toString()
+		return c
+	}
+	
+	
     def destroy = {
     }
 }
--- grails-app/controllers/chineseweb/CourseController.groovy
+++ grails-app/controllers/chineseweb/CourseController.groovy
@@ -1,6 +1,153 @@
 package chineseweb
+import grails.plugin.springsecurity.annotation.Secured
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+import chineseweb.security.*
 
+/**
+ * Manage courses
+ * @author lenovo
+ *
+ */
+@Transactional(readOnly = true)
+@Secured("ROLE_ADMIN")
 class CourseController {
+	def springSecurityService
 
-    def index() { }
+	/**
+	 * Course List
+	 * @return
+	 */
+	def index() {
+		User user=springSecurityService.currentUser
+		model:[list:user.courses]
+	}
+
+	/**
+	 * Show detailed information of a specific course
+	 * @param course
+	 * @return
+	 */
+	def show(Course course) {
+		[course:course]
+	}
+
+	/**
+	 * Ajax. Open create-new-course dialog
+	 * @return
+	 */
+	def create() {
+		Course c=new Course()
+		[courseInstance:c]
+	}
+
+	/**
+	 * Save a created course
+	 * @param courseInstance
+	 * @return
+	 */
+	@Transactional
+	def save(Course courseInstance) {
+		courseInstance.properties=params
+		def user=springSecurityService.currentUser
+		user.addToCourses(courseInstance).save(flush:true,failOnError:true)
+
+
+
+		if (courseInstance == null) {
+			notFound()
+			return
+		}
+		if (courseInstance.hasErrors()) {
+			respond courseInstance.errors, view:'create'
+			return
+		}
+
+		courseInstance.save flush:true
+
+		request.withFormat {
+			form multipartForm {
+				flash.message = "Course ${courseInstance.courseTitle} is created."
+				render view:"show",model:[course:courseInstance]
+			}
+			'*' { respond courseInstance, [status: CREATED] }
+		}
+	}
+
+	/**
+	 * Ajax. Edit a course
+	 * @param courseInstance
+	 * @return
+	 */
+	def edit(Course courseInstance) {
+		[courseInstance: courseInstance]
+	}
+
+
+	/**
+	 * Save an edited course
+	 * @param courseInstance
+	 * @return
+	 */
+	@Transactional
+	def update(Course courseInstance) {
+		if (courseInstance == null) {
+			notFound()
+			return
+		}
+
+		if (courseInstance.hasErrors()) {
+			respond courseInstance.errors, view:'edit'
+			return
+		}
+
+		courseInstance.save flush:true
+
+		request.withFormat {
+			form multipartForm {
+				flash.message = "Course ${courseInstance.courseTitle} is updated."
+				redirect courseInstance
+			}
+			'*'{ respond courseInstance, [status: OK] }
+		}
+	}
+
+	/**
+	 * Delete a specific course
+	 * @param courseInstance
+	 * @return
+	 */
+	@Transactional
+	def delete(Course courseInstance) {
+
+		if (courseInstance == null) {
+			notFound()
+			return
+		}
+
+		courseInstance.delete flush:true
+
+		request.withFormat {
+			form multipartForm {
+				flash.message = "Course ${courseInstance.courseTitle} is deleted. "
+				// redirect action:"index", method:"GET"
+			}
+			'*'{ render status: NO_CONTENT }
+		}
+		User user=springSecurityService.currentUser
+		render view:"index",model:[list:user.courses]
+	}
+
+	/**
+	 * Whatever...
+	 */
+	protected void notFound() {
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.not.found.message', args: [message(code: 'course.label', default: 'Course'), params.id])
+				redirect action: "index", method: "GET"
+			}
+			'*'{ render status: NOT_FOUND }
+		}
+	}
 }
